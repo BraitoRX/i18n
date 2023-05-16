61753928378bcfbdd0fe885bf92131564c8f68ee
Brian <b.riverah@uniandes.edu.co>
2023-05-16 09:24:02 -0500

    final

--- src/components/jobslist.js
+++ src/components/jobslist.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Job from "./job";
-
+import { FormattedMessage } from "react-intl";
 const JobsList = () => {
   const [offers] = useState([
     {
@@ -10,6 +10,7 @@ const JobsList = () => {
       salary: 4.5,
       city: "Bogotá, Colombia",
       date: "2019-03-26",
+      visits: 1250,
     },
     {
       id: "0002",
@@ -18,6 +19,7 @@ const JobsList = () => {
       salary: 20,
       city: "Palo Alto, CA, USA",
       date: "2019-03-27",
+      visits: 1251
     },
     {
       id: "0003",
@@ -26,6 +28,7 @@ const JobsList = () => {
       salary: 1,
       city: "Cali, Colombia",
       date: "2019-03-28",
+      visits: 1252
     },
   ]);
 
@@ -35,11 +38,16 @@ const JobsList = () => {
         <thead className="thead-dark">
           <tr>
             <th scope="col">#</th>
-            <th scope="col">Position</th>
+            <th scope="col">
+              <FormattedMessage id="Position" />
+            </th>
             <th scope="col">Company</th>
             <th scope="col">Salary</th>
             <th scope="col">City</th>
             <th scope="col">Publication date</th>
+            <th scope="col">
+              <FormattedMessage id="Views" />
+            </th> {/* New column for visits */}
           </tr>
         </thead>
         <tbody>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { IntlProvider } from "react-intl";
+import localeEsMessagesEn from "./locales/en.json";
+import localeEsMessagesEs from "./locales/es.json";
 import JobsList from "./components/jobslist";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+const locale = "en"; // Cambiar el valor de `locale` según sea necesario
+
+const messages = locale === "en" ? localeEsMessagesEn : localeEsMessagesEs;
+
+ReactDOM.render(
+  <IntlProvider locale={locale} messages={messages}>
+    <JobsList />
+  </IntlProvider>,
+  document.getElementById("root")
+);
